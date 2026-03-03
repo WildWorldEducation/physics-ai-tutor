626790efc4bf0b739fe1ce93e5ddc32d8f249633
RussiSunni <jonathandyason@hotmail.com>
2026-03-03 17:25:32 +0200

    .

--- Assets/Scripts/TTSResponseBehaviour.cs
+++ Assets/Scripts/TTSResponseBehaviour.cs
@@ -22,9 +22,9 @@ public class TTSResponseBehaviour : MonoBehaviour
     }
 
     public async void Response(string chatGPTResponse)
-    {      
+    {
         // Get a key using another account, not root.
-        var credentials = new BasicAWSCredentials(accessKey: "AKIA6RPUV6MB6ZNKUP2U", secretKey: awsSecret.text);
+        var credentials = new BasicAWSCredentials(accessKey: "AKIA6RPUV6MBZWYENGMV", secretKey: awsSecret.text);
         var client = new AmazonPollyClient(credentials, Amazon.RegionEndpoint.USEast1);
 
         var request = new SynthesizeSpeechRequest()
